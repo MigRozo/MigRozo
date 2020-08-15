2418c13e35eeb91a1be7f9c814672214d28716ab
MigRozo <angelorozo13@hotmail.com>
2020-08-14 20:22:52 -0500

    [add] Rellax module added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Rellax from 'rellax';
 import Header from './components/Header';
 import './App.css';
 
-const App = _ => (
-    <main className="App">
-        <Header />
+const App = _ => {
+    useEffect(_ => {
+        new Rellax('.rellax');
+    });
+    return (
+        <main className="App">
+            <Header />
 
-        <section className="intro">
-            <div className="container">
-                <div className="intro_row fx ac">
-                    <h1 className="intro_title titles">Mig<br/>Rozo <span>Frontend Developer</span></h1>
+            <section className="intro">
+                <div className="intro_container container">
+                    <div className="intro_row fx ac">
+                        <h1 className="intro_title titles">Mig<br/>Rozo
+                            <span>Frontend Developer</span>
+                            <svg className="hex" width="200" height="200" viewBox="0 0 100 100">
+                                <use xlinkHref="/svg/shapes.svg#hex" />
+                            </svg>
+                        </h1>
+                    </div>
+                    <svg className="intro_deco1 intro_deco rellax" width="30" height="30" viewBox="0 0 100 100" data-rellax-speed="2">
+                        <use xlinkHref="/svg/shapes.svg#hex" />
+                    </svg>
+                    <svg className="intro_deco2 intro_deco rellax" width="50" height="50" viewBox="0 0 100 100" data-rellax-speed="-2">
+                        <use xlinkHref="/svg/shapes.svg#hex" />
+                    </svg>
                 </div>
-            </div>
-            <a href="/" className="intro_scroll">
-                <svg width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-                    <text x="0" y="24">scroll</text>
-                </svg>
-            </a>
-        </section>
+                <a href="/" className="intro_scroll">
+                    <svg width="40" height="40">
+                        <svg className="hex" width="40" height="40" viewBox="0 0 100 100">
+                            <use xlinkHref="/svg/shapes.svg#hex" />
+                        </svg>
+                        <svg className="tri" width="16" height="16" x="12" y="12" viewBox="0 0 100 100">
+                            <use xlinkHref="/svg/shapes.svg#tri" />
+                        </svg>
+                    </svg>
+                </a>
+            </section>
 
-        {/* <footer className="footer">footer</footer> */}
-    </main>
-);
+            <section className="">About</section>
+            <section className="">Work</section>
+            <section className="">
+                <div className="container">
+                    <p>Contact</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex necessitatibus alias accusantium nobis nemo aperiam numquam aut fuga soluta magnam sequi incidunt pariatur exercitationem, laboriosam, officia dolores hic suscipit doloremque illum. Sint quaerat quidem illum eligendi natus quasi dolorum, accusamus maiores inventore voluptatum fugiat dolor dolores fugit sit ipsam repellendus.</p>
+                </div>
+            </section>
+
+            {/* <footer className="footer">footer</footer> */}
+        </main>
+    )
+};
 
 export default App;
