a0ad73ece427045425d34cbd8a65ea5b60646f7f
Mig Rozo <angelorozo13@hotmail.com>
2020-08-18 18:51:35 -0500

    Updates

---
+++ precache-manifest.b68d025ce00c4c98b109b20e375d9bbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2270717ff01f28be362c69525f2174a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee2f76670533ba9d0bc9",
+    "url": "/static/css/main.ff53fe14.chunk.css"
+  },
+  {
+    "revision": "4f8c22270868d2e11090",
+    "url": "/static/js/2.a7c43f3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7c43f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2f76670533ba9d0bc9",
+    "url": "/static/js/main.ea4a2287.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
