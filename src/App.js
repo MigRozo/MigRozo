774880664231f900472dbaa86878944565fc00d8
Mig Rozo <angelorozo13@hotmail.com>
2020-08-19 18:59:57 -0500

    [add] Contact section added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Rellax from 'rellax';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact, faJs, faCss3Alt, faHtml5, faGitAlt, faGithub, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { faEnvelope, faHeart } from '@fortawesome/free-regular-svg-icons'
 import './App.css';
 
 const App = _ => {
@@ -58,7 +58,7 @@ const App = _ => {
                             <div className="tech">
                                 <ul className="tech_list fx ac ja listReset">
                                     <li className="tech_item">
-                                        <span className="tech_icon" title="React">
+                                        <span className="tech_icon" title="React JS">
                                             <svg className="hex" width="40" height="40" viewBox="0 0 100 100">
                                                 <use xlinkHref="/svg/shapes.svg#hex" />
                                             </svg>
@@ -125,29 +125,59 @@ const App = _ => {
                     <div className="sect_row fx ac">
                         <div className="sect_col">
                             <h2 className="contact_title titles">Contact</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam laborum nam debitis.</p>
+                            <p>For more information, you can write me an email or direct message on one of these networks.</p>
                             <div className="social">
                                 <ul className="social_list fx ac listReset">
                                     <li className="social_item">
-                                        <a href="/" className="social_link"><FontAwesomeIcon icon={faGithub} /></a>
+                                        <a href="https://github.com/MigRozo" target="_blank" rel="noopener noreferrer" className="social_link" title="GitHub">
+                                            <svg className="hex" width="40" height="40" viewBox="0 0 100 100">
+                                                <use xlinkHref="/svg/shapes.svg#hex" />
+                                            </svg>
+                                            <FontAwesomeIcon icon={faGithub} />
+                                        </a>
                                     </li>
                                     <li className="social_item">
-                                        <a href="/" className="social_link"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                                        <a href="https://co.linkedin.com/public-profile/in/miguel-angel-rozo-546b1784" target="_blank" rel="noopener noreferrer" className="social_link" title="LinkedIn">
+                                            <svg className="hex" width="40" height="40" viewBox="0 0 100 100">
+                                                <use xlinkHref="/svg/shapes.svg#hex" />
+                                            </svg>
+                                            <FontAwesomeIcon icon={faLinkedinIn} />
+                                        </a>
                                     </li>
                                     <li className="social_item">
-                                        <a href="/" className="social_link"><FontAwesomeIcon icon={faTwitter} /></a>
+                                        <a href="https://twitter.com/migrozo" target="_blank" rel="noopener noreferrer" className="social_link" title="Twitter">
+                                            <svg className="hex" width="40" height="40" viewBox="0 0 100 100">
+                                                <use xlinkHref="/svg/shapes.svg#hex" />
+                                            </svg>
+                                            <FontAwesomeIcon icon={faTwitter} />
+                                        </a>
                                     </li>
                                     <li className="social_item">
-                                        <a href="/" className="social_link"><FontAwesomeIcon icon={faEnvelope} /></a>
+                                        <a href="mailto:angelorozo13@hotmail.com" className="social_link" title="Email">
+                                            <svg className="hex" width="40" height="40" viewBox="0 0 100 100">
+                                                <use xlinkHref="/svg/shapes.svg#hex" />
+                                            </svg>
+                                            <FontAwesomeIcon icon={faEnvelope} />
+                                        </a>
                                     </li>
                                 </ul>
                             </div>
                         </div>
                     </div>
+                    <svg className="contact_deco1 deco rellax" width="50" height="50" viewBox="0 0 100 100" data-rellax-speed="1">
+                        <use xlinkHref="/svg/shapes.svg#hex" />
+                    </svg>
+                    <svg className="contact_deco2 deco rellax" width="30" height="30" viewBox="0 0 100 100" data-rellax-speed="-3">
+                        <use xlinkHref="/svg/shapes.svg#hex" />
+                    </svg>
                 </div>
             </section>
 
-            <footer className="footer">footer</footer>
+            <footer className="footer">
+                <div className="container">
+                    Made with <FontAwesomeIcon icon={faHeart} title="Love" /> in <span className="co" title="Colombia">CO</span> by <strong>Mig Rozo</strong>
+                </div>
+            </footer>
         </main>
     )
 };
