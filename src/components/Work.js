b49404f2ea9ede7eaea5578c9e0049688ba6517f
Mig Rozo <angelorozo13@hotmail.com>
2020-08-26 13:32:52 -0500

    [del] Projects firebase REST API reverted

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,18 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const Work = _ => {
-	const [projects, setProjects] = useState([]);
-	const API = 'https://migrozo-9cc15.firebaseio.com/projects.json';
-	useEffect(() => {
-		fetch( API )
-			.then( resp => resp.json() )
-			.then( result => {
-				setProjects(result);
-			}, err => {
-				console.error('Fetch Error: ', err)
-			});
-	}, []);
-
 	return (
 		<section id="work" className="work">
 			<div className="container">
@@ -21,16 +9,48 @@ const Work = _ => {
 						<h2 className="work_title titles">Work</h2>
 						<div className="work_pics">
 							<ul className="work_list listReset">
-								{
-									projects.map(({ name, site, image }, k) => (
-										<li className="work_item" key={k}>
-											<a href={site} target="_blank" className="work_link" rel="noopener noreferrer">
-												<img src={image} alt={name} loading="lazy" />
-												<span>{name}</span>
-											</a>
-										</li>
-									))
-								}
+								<li className="work_item">
+									<a href="https://www.hotellucent.com/" target="_blank" className="work_link" rel="noopener noreferrer">
+										<img src="https://drive.google.com/uc?export=view&id=1YbPE7zZPJl5Sh7myBunrFg3I_4DCvXHK" alt="Project Desc" loading="lazy" />
+										<span>Hotel Lucent</span>
+									</a>
+								</li>
+								<li className="work_item">
+									<a href="https://www.stanfordinnanaheim.com/" target="_blank" className="work_link" rel="noopener noreferrer">
+										<img src="https://drive.google.com/uc?export=view&id=1fX33UEzQBQ6j-IzcbjBkANyS7SgXgw5i" alt="Project Desc" loading="lazy" />
+										<span>Stanford Inn</span>
+									</a>
+								</li>
+								<li className="work_item">
+									<a href="https://www.sparrowbar.com/" target="_blank" className="work_link" rel="noopener noreferrer">
+										<img src="https://drive.google.com/uc?export=view&id=1rjJNyGHhNPY6YiL6oLEGnMI5Qa1Drs-W" alt="Project Desc" loading="lazy" />
+										<span>Sparrow Bar</span>
+									</a>
+								</li>
+								<li className="work_item">
+									<a href="https://www.papajohns.com.co/productos/detalle/por-mitades" target="_blank" className="work_link" rel="noopener noreferrer">
+										<img src="https://drive.google.com/uc?export=view&id=1dzJCV-TIX3bZTIuWJpB_uXSzYVO2Dcd-" alt="Papa John's" loading="lazy" />
+										<span>Papa John's</span>
+									</a>
+								</li>
+								<li className="work_item">
+									<a href="https://www.volo.works/" target="_blank" className="work_link" rel="noopener noreferrer">
+										<img src="https://drive.google.com/uc?export=view&id=11YsUXrm1prN1Y9xWKKPcASxR87LIiBjO" alt="Project Desc" loading="lazy" />
+										<span>Volo Works</span>
+									</a>
+								</li>
+								<li className="work_item">
+									<a href="https://www.eltecho.co/" target="_blank" className="work_link" rel="noopener noreferrer">
+										<img src="https://drive.google.com/uc?export=view&id=1ZEmCzfyV1vWNJLi-O6LhroN1UOe9eJYb" alt="Project Desc" loading="lazy" />
+										<span>El Techo</span>
+									</a>
+								</li>
+								<li className="work_item">
+									<a href="http://www.marominga.com/" target="_blank" className="work_link" rel="noopener noreferrer">
+										<img src="https://drive.google.com/uc?export=view&id=14qLZ2peRU8Jmk9X19DzD1eMLjHpXxHz6" alt="Marominga" loading="lazy" />
+										<span>Marominga</span>
+									</a>
+								</li>
 							</ul>
 						</div>
 					</div>
@@ -46,7 +66,7 @@ const Work = _ => {
 				</svg>
 			</div>
 		</section>
-	)
+	);
 };
 
 export default Work;
